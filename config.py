0a82f8a4dd7375d1e02c4e0259910d85b0184f54
Galia Adelshin <gadelshin@ctisteam.com>
2024-11-26 20:02:55 -0500

    unit tests

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import pathlib
 import os
 
-project_absolute_path = pathlib.Path(__file__).parent.absolute()
+# Get the project directory dynamically
+BASE_DIR = pathlib.Path(__file__).parent.resolve()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', f"sqlite:///{BASE_DIR / 'app.db'}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = 587
@@ -13,3 +14,8 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
+
+
+class TestConfig(Config):
+    # Use an in-memory SQLite database for testing
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
